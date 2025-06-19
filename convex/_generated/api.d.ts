7be6523e79117be4faedd280c5165299375833ce
Subho <contact.subhoghosh@gmail.com>
2025-06-19 22:55:06 +0530

    save

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,16 +8,18 @@
  * @module
  */
 
+import type * as messages from "../messages.js";
+import type * as promote from "../promote.js";
+import type * as streaming from "../streaming.js";
+import type * as temporary_messages from "../temporary_messages.js";
+import type * as temporary_threads from "../temporary_threads.js";
+import type * as threads from "../threads.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as messages from "../messages.js";
-import type * as promote from "../promote.js";
-import type * as temporary_messages from "../temporary_messages.js";
-import type * as temporary_threads from "../temporary_threads.js";
-import type * as threads from "../threads.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -30,15 +32,56 @@ import type * as threads from "../threads.js";
 declare const fullApi: ApiFromModules<{
   messages: typeof messages;
   promote: typeof promote;
+  streaming: typeof streaming;
   temporary_messages: typeof temporary_messages;
   temporary_threads: typeof temporary_threads;
   threads: typeof threads;
 }>;
+declare const fullApiWithMounts: typeof fullApi;
+
 export declare const api: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {
+  persistentTextStreaming: {
+    lib: {
+      addChunk: FunctionReference<
+        "mutation",
+        "internal",
+        { final: boolean; streamId: string; text: string },
+        any
+      >;
+      createStream: FunctionReference<"mutation", "internal", {}, any>;
+      getStreamStatus: FunctionReference<
+        "query",
+        "internal",
+        { streamId: string },
+        "pending" | "streaming" | "done" | "error" | "timeout"
+      >;
+      getStreamText: FunctionReference<
+        "query",
+        "internal",
+        { streamId: string },
+        {
+          status: "pending" | "streaming" | "done" | "error" | "timeout";
+          text: string;
+        }
+      >;
+      setStreamStatus: FunctionReference<
+        "mutation",
+        "internal",
+        {
+          status: "pending" | "streaming" | "done" | "error" | "timeout";
+          streamId: string;
+        },
+        any
+      >;
+    };
+  };
+};
